241fceff69c533f228ac687aaecb20c308a15d48
unknown <student1@FB-W206-W8.academic.touro.edu>
2015-09-17 20:02:49 -0400

    modified Pythagorean Theorem Test

--- src/golub/pythagoreanTheorem/PythagoreanTheorem.java
+++ src/golub/pythagoreanTheorem/PythagoreanTheorem.java
@@ -17,14 +17,14 @@ public class PythagoreanTheorem {
 	}
 
 	public void setAC(double a, double c) throws InvalidDataException {
-		if (a <= 0 || c <= 0 || a < c) {
+		if (a <= 0 || c <= 0 || a > c) {
 			throw new InvalidDataException();
 		}
 		this.b = Math.sqrt((c * c) - (a * a));
 	}
 
 	public void setBC(double b, double c) throws InvalidDataException {
-		if (b <= 0 || c <= 0 || b < c) {
+		if (b <= 0 || c <= 0 || b > c) {
 			throw new InvalidDataException();
 		}
 		this.a = Math.sqrt((c * c) - (b * b));
