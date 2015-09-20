0be46b8a528c180d25507a6c75a6b31bdb6301e7
unknown <mgolub829@gmail.com>
2015-09-20 18:35:54 -0400

    Updated ScrabbleDictionary with HashSet

--- src/golub/scrabble/ScrabbleDictionary.java
+++ src/golub/scrabble/ScrabbleDictionary.java
@@ -2,26 +2,26 @@ package golub.scrabble;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ScrabbleDictionary {
+	
+	private HashSet<String> dictionary = new HashSet<String>();
+
 	public ScrabbleDictionary() {
-
-	}
-
-	public boolean contains(String word) {
 		Scanner input;
 		try {
 			input = new Scanner(new File("US.dic"));
 			while (input.hasNext()) {
-				String compareWord = input.next();
-				if (word.equalsIgnoreCase(compareWord)) {
-					return true;
-				}
+				dictionary.add(input.next());
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found. Please check connection.");
 		}
-		return false;
+	}
+
+	public boolean contains(String word) {
+		return dictionary.contains(word);
 	}
 }
